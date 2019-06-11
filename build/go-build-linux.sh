19ba02ff78868b35d43ebe936b5fd963c1a5e2b3
Oluchi Enebeli <Oluchi.Enebeli@Sterling.ng>
2019-06-11 14:59:29 +0100

    cleaning up codebase

--- build/go-build-linux.sh
+++ build/go-build-linux.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o build/t24Fix_interface.elf -ldflags "-s -w" && upx stib_linux.elf
-
-#function pause(){
-#   read -p "$*"
-#}
-#pause 'Press [Enter] key to continue...'
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o build/app.elf -ldflags "-s -w" && upx app.elf
--- build/go-build-mac.sh
+++ build/go-build-mac.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-#go build  -o stib_mac.app -ldflags "-s -w" && mv stib_mac.app app/.
-go build  -o build/t24Fix_interface.app -ldflags "-s -w"
-
-function pause(){
-   read -p "$*"
-}
-pause 'Press [Enter] key to continue...'
+go build  -o build/app.app -ldflags "-s -w"
